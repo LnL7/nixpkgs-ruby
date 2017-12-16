21617bd672027b7fa1be0696f329f4a4eb4e4678
Daiderd Jordan <daiderd@gmail.com>
2017-12-16 12:12:44 +0100

    move interpreters

--- ruby-overlay.nix
+++ ruby-overlay.nix
@@ -6,20 +6,20 @@ in
 
 {
   ruby-overlay = rec {
-    interpreters.ruby_1_8_7 = rec {
-      ruby = callPackage ./ruby-1.8.7/ruby.nix {};
-      rubygems = callPackage ./ruby-1.8.7/rubygems.nix { inherit ruby; };
-      buildRubyGem = callPackage ./ruby-1.8.7/build-gem.nix { inherit ruby rubygems; };
+    interpreters = {
+      ruby_1_8_7 = callPackage ./ruby-1.8.7/ruby.nix {};
     };
 
     packages.ruby_1_8_7 = rec {
-      bundler = super.bundler.override {
-        inherit (interpreters.ruby_1_8_7) ruby buildRubyGem;
-      };
+      ruby = interpreters.ruby_1_8_7;
+      rubygems = callPackage ./ruby-1.8.7/rubygems.nix { inherit ruby; };
+      buildRubyGem = callPackage ./ruby-1.8.7/build-gem.nix { inherit ruby rubygems; };
+
+      bundler = super.bundler.override { inherit ruby buildRubyGem; };
     };
   };
 
-  buildRubyGem_1_8_7 = self.ruby-overlay.interpreters.ruby_1_8_7.buildRubyGem;
-  ruby_1_8_7 = self.ruby-overlay.interpreters.ruby_1_8_7.ruby;
-  rubygems = self.ruby-overlay.interpreters.ruby_1_8_7.rubygems;
+  buildRubyGem_1_8_7 = self.ruby-overlay.packages.ruby_1_8_7.buildRubyGem;
+  ruby_1_8_7 = self.ruby-overlay.packages.ruby_1_8_7.ruby;
+  rubygems = self.ruby-overlay.packages.ruby_1_8_7.rubygems;
 }
