91513edbad944a89f80ee3af7442296e0cc0c01a
Daiderd Jordan <daiderd@gmail.com>
2017-12-31 16:16:35 +0100

    recurse into packages

--- ruby-overlay.nix
+++ ruby-overlay.nix
@@ -10,12 +10,14 @@ in
       ruby_1_8_7 = callPackage ./ruby-1.8.7/ruby.nix {};
     };
 
-    packages.ruby_1_8_7 = recurseIntoAttrs rec {
-      ruby = interpreters.ruby_1_8_7;
-      rubygems = callPackage ./ruby-1.8.7/rubygems.nix { inherit ruby; };
-      buildRubyGem = callPackage ./ruby-1.8.7/build-gem.nix { inherit ruby rubygems; };
+    packages = recurseIntoAttrs {
+      ruby_1_8_7 = recurseIntoAttrs rec {
+        ruby = interpreters.ruby_1_8_7;
+        rubygems = callPackage ./ruby-1.8.7/rubygems.nix { inherit ruby; };
+        buildRubyGem = callPackage ./ruby-1.8.7/build-gem.nix { inherit ruby rubygems; };
 
-      bundler = super.bundler.override { inherit ruby buildRubyGem; };
+        bundler = super.bundler.override { inherit ruby buildRubyGem; };
+      };
     };
   };
 
